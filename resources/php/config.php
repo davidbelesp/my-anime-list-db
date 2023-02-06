c9639db0c9634494e93a2da63dba28b4ba58384a
David <dbeltrane01@iesarroyoharnina.es>
2023-02-06 22:58:53 +0100

    Client Update

--- resources/php/config.php
+++ resources/php/config.php
@@ -1,8 +1,11 @@
 <?php
 
-$CLIENT_ID = "";
+$CLIENT_ID = "YmRmNTg0NjE5MzRkYzg2YWNjN2ViNjg1M2U2M2UyOWU";
 $URL = "";
-$USERNAME = "";
+$USERNAME = "example";
+
+// $CLIENT_ID = "YmRmNTg0NjE5MzRkYzg2YWNjN2ViNjg1M2U2M2UyOWU";
+// $URL = "";
+// $USERNAME = "example"; 
 
 ?> 
-
